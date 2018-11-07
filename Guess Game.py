59a6ead1db39291536b3872090caee118036de5f
Moej54m <42385567+Moej54m@users.noreply.github.com>
2018-11-07 09:47:15 -0800

    The Guess Game Finished

---
+++ Guess Game.py
@@ -0,0 +1,18 @@
+import random
+
+r = random.randint(0, 10)
+guesses_left = 5
+Playing = True
+print("Think of a number from 1 to 10")
+
+while guesses_left > 0 and playing:
+    guess = int(input("Guess="))
+    if guess > r:
+        print("Lower")
+        Guesses_left = 1
+    elif guess < r:
+        print("Greater")
+        Guesses_left -= 1
+    else:
+        print("Correct")
+        playing = False
