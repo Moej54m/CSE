e6adaee6c05c48bf8bacba7c2e12722a7fca5225
Moej54m <42385567+Moej54m@users.noreply.github.com>
2018-11-15 09:48:51 -0800

    Added things?

--- Guess Game.py
+++ Guess Game.py
@@ -7,6 +7,7 @@ print("Think of a number from 1 to 10")
 
 while guesses_left > 0 and playing:
     guess = int(input("Guess="))
+
     if guess > r:
         print("Lower")
         guesses_left = guesses_left -1
@@ -15,7 +16,7 @@ while guesses_left > 0 and playing:
         guesses_left = guesses_left -1
     else:
         print("Correct")
-        playing = Falsea
+        playing = False
     if guesses_left == 0:
         print("You Lose")
 
---
+++ Lucky 7s.py
@@ -0,0 +1,33 @@
+import random
+money = 15
+rounds = 0
+most = 15
+
+while money >= 1 and money < 30:
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+
+    if dice1 + dice2 == 7:
+        money += 5
+        print("You got 7")
+        print(money)
+        print()
+        rounds += 1
+
+    if dice1 + dice2 > 7 or dice1 + dice2 < 7:
+        print("You did not get seven")
+        money -= 1
+        print(money)
+        print("You rolled a")
+        print(dice1 and dice2)
+        print()
+        rounds += 1
+    if money > most:
+            most = money
+
+print("You played")
+print(rounds)
+print()
+print("Your greatest amount of money was")
+print(most)
+
