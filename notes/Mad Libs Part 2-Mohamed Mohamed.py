e75aa91b767e7ab98d30ddf8857047400371b485
Moej54m <42385567+Moej54m@users.noreply.github.com>
2018-12-04 09:46:03 -0800

    12.4.18

---
+++ notes/Mad Libs Part 2-Mohamed Mohamed.py
@@ -0,0 +1,15 @@
+list = []
+list.append(input("Enrer a noun"))
+list.append(input("Enter a name"))
+list.append(input("Enter a noun"))
+list.append(input("Enter a noun"))
+list.append(input("Enter a noun"))
+list.append(input("Enter an adjective"))
+list.append(input("Enter a body part"))
+list.append(input("Enter a noun"))
+list.append(input("Enter a noun"))
+list.append(input("Enter a noun"))
+print("On %s, %s, who was the CEO of %s introduced the revolutionary %s. It has %s gestures, being able to "
+      "have a touch screen was %s It Works like Magic they said. Using your %s instead of a keyboard or stylus made "
+      "it extra special than the average %s like a %s. This makes the %s"
+      "amazing." % (list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[7], list[8], list[9]))
