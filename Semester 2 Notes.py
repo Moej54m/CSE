60601af58b3bdeb676c7311f22aaa47101235ebe
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-01-08 09:46:01 -0800

    1.8.19

---
+++ Semester 2 Notes.py
@@ -0,0 +1,11 @@
+print ("Hello World")
+# nsdconcsdkj sjksd sDiobv
+cars = 5
+driving = True
+print("I have %d cars" % cars)
+age = input("How old are you?")
+print(age + "???. Don't lie.")
+colors = ["red", "orange", "yellow", "green", "blue"]
+colors.append("white")
+colors.pop(0)
+print(colors[])
