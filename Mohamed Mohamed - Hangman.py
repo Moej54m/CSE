a348f80d3ca46c7809248c6d5680ff55e5422d3a
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-01-10 09:45:53 -0800

    1.10.2019

---
+++ Mohamed Mohamed - Hangman.py
@@ -0,0 +1,2 @@
+athletes = ["LeBron James", "Kyrie Irving", "National Basketball Association.", "Doofenshmirtz Evil Incorporated.", "Wakanda Forever.",
+            "High School", "Hamburger", "Russell Westbrook", "Games"]
