5d5d27ee1af70a0711cfe6e209cb70b4cdb8bd58
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-01-16 09:44:33 -0800

    1.16.19

--- Mohamed Mohamed - Hangman.py
+++ Mohamed Mohamed - Hangman.py
@@ -1,3 +1,10 @@
-athletes = ["LeBron James", "Kyrie Irving", "National Basketball Association.", "Doofenshmirtz Evil Incorporated.", "Wakanda Forever.",
-            "High School", "Hamburger", "Russell Westbrook", "Games"]
+import random
+import string
+athletes = ["LeBron James", "Kyrie Irving", "National Basketball Association.", "Doofenshmirtz Evil Incorporated.",
+            "Wakanda Forever.", "High School", "Hamburger", "Russell Westbrook", "Games"]
 print("I'm thinking of a word, what is it?")
+
+word = random.choice(athletes)
+print(word)
+print(list(string.punctuation))
+print(string.whitespace)
--- Semester 2 Notes.py
+++ Semester 2 Notes.py
@@ -1,5 +1,5 @@
 print ("Hello World")
-# nsdconcsdkj sjksd sDiobv
+ nsdconcsdkj sjksd sDiobv
 cars = 5
 driving = True
 print("I have %d cars" % cars)
@@ -9,3 +9,8 @@ colors = ["red", "orange", "yellow", "green", "blue"]
 colors.append("white")
 colors.pop(0)
 print(colors[])
+
+import string
+print(list(string.ascii_letters))
+print(string.digits)
+print(string.punctuation)
