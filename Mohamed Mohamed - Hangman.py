2524f861c11ea4f376937172a369c809200b1e46
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-01-18 10:13:36 -0800

    1.18.2019

--- Mohamed Mohamed - Hangman.py
+++ Mohamed Mohamed - Hangman.py
@@ -1,10 +1,38 @@
 import random
 import string
-athletes = ["LeBron James", "Kyrie Irving", "National Basketball Association.", "Doofenshmirtz Evil Incorporated.",
+words = ["LeBron James", "Kyrie Irving", "National Basketball Association.", "Doofenshmirtz.",
             "Wakanda Forever.", "High School", "Hamburger", "Russell Westbrook", "Games"]
-print("I'm thinking of a word, what is it?")
+output = []
+guesses = 8
 
-word = random.choice(athletes)
-print(word)
-print(list(string.punctuation))
-print(string.whitespace)
+list_of_letters = string.ascii_letters
+
+word_selection = random.choice(words)
+length = len(word_selection)
+word_list = list(word_selection)
+
+for i in range(length):
+    output.append("* ")
+print(word_selection)
+print("".join(output))
+
+while guesses > 0 and len(word_list) > 0:
+    user_guess = input("Guess a letter")
+    print('\n' * 10)
+    if user_guess in word_selection:
+        print("You are correct!")
+        for i in range (len(word_selection)):
+            if user_guess in word_list:
+                word_list.pop(i)
+    for i in range(len(word_selection)):
+        if word_selection[i] == user_guess:
+            output.pop(i)
+            output.insert(i, user_guess)
+        print("".join(output))
+    else:
+        print("WRONG")
+        guesses = (guesses - 1)
+        print(guesses)
+    if guesses <= 0:
+        print("You are CORRRRRECT!")
+        print("The word was %s" % word_selection)
