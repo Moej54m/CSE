527cfdc92691c57f515800b9f540e286ddad27d5
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-01-24 09:44:52 -0800

    1.24.19

--- Mohamed Mohamed - Hangman.py
+++ Mohamed Mohamed - Hangman.py
@@ -4,6 +4,7 @@ words = ["LeBron James", "Kyrie Irving", "National Basketball Association.", "Do
             "Wakanda Forever.", "High School", "Hamburger", "Russell Westbrook", "Games"]
 output = []
 guesses = 8
+guess_word = []
 
 list_of_letters = string.ascii_letters
 
@@ -12,15 +13,15 @@ length = len(word_selection)
 word_list = list(word_selection)
 
 for i in range(length):
-    output.append("* ")
+    guess_word.append("*")
 print("".join(output))
 
 while guesses > 0 and len(word_list) > 0:
-    user_guess = input("Guess a letter")
+    user_guess = input("Guess a letter:")
     print('\n' * 10)
     if user_guess in word_selection:
         print("You are correct!")
-        for i in range (len(word_selection)):
+        for i in range(len(word_selection)):
             if user_guess in word_list:
                 word_list.pop(i)
     for i in range(len(word_selection)):
@@ -32,6 +33,6 @@ while guesses > 0 and len(word_list) > 0:
         print("WRONG")
         guesses = (guesses - 1)
         print(guesses)
-    if guesses <= 0:
+    if guesses == words:
         print("You are CORRRRRECT!")
         print("The word was %s" % word_selection)
