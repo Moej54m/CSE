198725845fba4a40fe68604cda11f7604a7a6d4f
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-01-28 09:25:55 -0800

    DONE WITH HANGMAN

--- Mohamed Mohamed - Hangman.py
+++ Mohamed Mohamed - Hangman.py
@@ -1,6 +1,6 @@
 import random
 import string
-words = ["LeBron James", "Kyrie Irving", "National Basketball Association.", "Doofenshmirtz.",
+words = ["LeBron James", "Kyrie Irving", "NBA.", "Doofenshmirtz.",
             "Wakanda Forever.", "High School", "Hamburger", "Russell Westbrook", "Games"]
 output = []
 guesses = 8
@@ -11,29 +11,38 @@ list_of_letters = string.ascii_letters
 word_selection = random.choice(words)
 length = len(word_selection)
 word_list = list(word_selection)
-print(word_list)
+punctuation = string.punctuation
+list_of_punctuation = list(punctuation)
 
 for i in range(length):
     output.append("* ")
+
+for i in range(length):
+    for j in range(len(list_of_punctuation)):
+        if list_of_punctuation[j] in word_selection[i]:
+            output.pop(i)
+            output.insert(i, list_of_punctuation[j])
+
 print("".join(output))
 
 while guesses > 0 and len(word_list) > 0:
     user_guess = input("Guess a letter:")
     print('\n' * 10)
-    print(output)
-    if user_guess.lower() in word_list or user_guess.upper() in word_list:
+    if user_guess.lower() in word_selection or user_guess.upper() in word_selection:
         print("You are correct!")
         for i in range(len(word_selection)):
             if word_selection[i].lower() == user_guess.lower():
                 output.pop(i)
                 output.insert(i, user_guess)
         print("".join(output))
-
     else:
-        print("NOOPE")
+        print("You got it wrong!")
         guesses = (guesses - 1)
         print(guesses)
-        print("You are CORRRRRECT!")
-
-    if guesses == words:
-        print("The word was %s" % word_selection)
+    if guesses <= 0:
+        print("You don't have any guesses. GAME OVER")
+        break
+    if output == word_list:
+        print("You won!!!")
+        print("THe word was %s" % word_selection)
+        break
