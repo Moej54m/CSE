58cba5702b8735fd261c2db581c2aff4af1cab6c
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-02-06 09:44:21 -0800

    2/6/19 MAP

--- Dictionary World Map- Mohamed Mohamed.py
+++ Dictionary World Map- Mohamed Mohamed.py
@@ -1,27 +1,35 @@
-world_map = {
-    'R19A': {
-        'NAME': "Mr. Wiebe's Room",
-        'DESCRIPTION': "This is the classroom you are in right now. "
-                       "There are two doors on the north wall.",
+world map = {
+    'Nuketown Hospital': {
+        'NAME': "Lobby",
+        'DESCRIPTION': " You are now in Nuketown hospital. You are inside the lobby of the hospital. There is one the",
+                       " door of each of the east, west, and north wall."
         'PATHS': {
-            'NORTH': "Parking_LOT"
-        }
-    },
-    'PARKING_LOT': {
-        'NAME': "The North Parking Lot",
-        'DESCRIPTION': "There are a couple cars parked here.",
-        'PATHS': {
-            'SOUTH': 'R19A'
+            'NORTH': '1st_Hallway',
+            'WEST': 'Stairs',
+            'EAST': 'Waiting_Room'
+
 
         }
-    }
+    },
+    '1st_Hallway': {
+        'NAME': "The First Hallway",
+        'DESCRIPTION': "You are now in the hallway of the first floor. There are doors that are boarded up in the east",
+                       " and west and cannot be pried open."
+                       "The only thing you see is a beautiful, red, hallway runner placed on the floor."
+        'PATHS':{
+            'SOUTH': 'Lobby'
+
+        }
+    },
+    'Stairs': {
+        'NAME': "Main Floor Stairs",
+        'DESCRIPTION': "These are the stairs for the first floor. The appear to lead up to the second floor."
+        'PATHS': {
+            'EAST': 'Lobby'
+        }
 }
 
 
-world map = {
-    'Nuketown Hospital': {
-        'NAME': "Lobby Room",
-        'DESCRIPTION': " This is the name of the hospital you are in"
 
 
 
