249382b5484a62711a57d00ce1febb95f689551b
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-02-07 09:45:44 -0800

    2/7/19

---
+++ Controller.py
@@ -0,0 +1,20 @@
+playing = true
+
+playing = True
+current_node = world_map['LOBBY'
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
+
+while playing:
+    print(current_node['NAME'])
+    print(current_node['DESCRIPTION'])
+    command = input("> ")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except keyError:
+            print("You can't go that way")
+        else:
+            print("Command Not Found")
--- Dictionary World Map- Mohamed Mohamed.py
+++ Dictionary World Map- Mohamed Mohamed.py
@@ -1,36 +1,52 @@
-world map = {
-    'Nuketown Hospital': {
+import os
+world_map = {
+    'LOBBY': {
         'NAME': "Lobby",
         'DESCRIPTION': " You are now in Nuketown hospital. You are inside the lobby of the hospital. There is one the",
                        " door of each of the east, west, and north wall."
         'PATHS': {
-            'NORTH': '1st_Hallway',
-            'WEST': 'Stairs',
-            'EAST': 'Waiting_Room'
+            'NORTH': '1ST_HALLWAY',
+            'WEST': 'STAIRS',
+            'EAST': 'WAITING_ROOM'
 
 
         }
     },
-    '1st_Hallway': {
+    '1ST_HALLWAY': {
         'NAME': "The First Hallway",
-        'DESCRIPTION': "You are now in the hallway of the first floor. There are doors that are boarded up in the east",
+        'DESCRIPTION': "You are now in the hallway of the first floor. There are doors that are boarded up in the east,"
                        " and west and cannot be pried open."
-                       "The only thing you see is a beautiful, red, hallway runner placed on the floor."
+                       "The only thing you see is a beautiful, red, hallway runner placed on the floor.",
         'PATHS':{
-            'SOUTH': 'Lobby'
+            'SOUTH': 'LOBBY'
 
         }
     },
-    'Stairs': {
+    'STAIRS': {
         'NAME': "Main Floor Stairs",
-        'DESCRIPTION': "These are the stairs for the first floor. The appear to lead up to the second floor."
+        'DESCRIPTION': "These are the stairs for the first floor. The appear to lead up to the second floor.",
         'PATHS': {
-            'EAST': 'Lobby'
+            'EAST': 'LOBBY'
         }
-}
-
-
-
+    },
+    'WAITING_ROOM': {
+        'NAME': 'The Waiting Room',
+        'DESCRIPTION': "This is the Waiting Room. You don't see nothing except chairs and a table.",
+        'PATHS':{
+            'NORTH': 'CAFETERIA',
+            'WEST': 'LOBBY'
+    },
+    'KITCHEN': {
+        'NAME': 'The Kitchen',
+        'DESCRIPTION': "You see a lot of drawer and cabinets. There are also utensils on some tables. You see an oven and also a pantry.",
+        'PATHS':{
+            'SOUTH': 'CAFETERIA'
+    },
+    'CAFETERIA': {
+        'NAME': 'The Cafeteria'
+        'DESCRIPTION': "You see a lot of tables. There are also many empty restaurants. As you look closer, each restaurant has a big closet.",
+        'PATHS':
+    },
 
 
 
