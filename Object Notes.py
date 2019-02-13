70567843a973c7ce6688d9ad829c41d060eda0fb
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-02-13 09:43:04 -0800

    2/13/19

--- Object Notes.py
+++ Object Notes.py
@@ -1,5 +1,8 @@
+import Special_Random
+
+
 class Phone(object):
-   def __init__(self):(self,carrier, charge_left):
+   def __init__(self, carrier, charge_left=50):
        # These are attributes that a phone has.
        # These should all be relevant to our program
        self.screen = True
@@ -8,31 +11,44 @@ class Phone(object):
        self.carrier = carrier
        self.battery_left = charge_left
 
-   def charge(self, time):
+    def charge(self, time):
         self.battery_left += time
         if self.battery_left > 100:
             self.battery_left = 100
 
-    def make_call(self_duration):
+   def make_call(self, duration):
         if not self.screen:
             print("You can't make a phone call.")
             print("Your screen is broken.")
             return
         battery_loss_per_minute = 5
         if self.battery_left <= 0:
-        print("You can't. The phone is dead.")
-        return
+            print("You can't. The phone is dead.")
+            return
         self.battery_left -= duration * battery_loss_per_minute
         if self.battery_left < 0:
             self.battery_left = 0
-        print("Your phone dies during the conversation.")
+            print("Your phone dies during the conversation.")
         elif self.battery_left == 0:
-        print("Your phone dies at the end of the conversation.")
+             print("Your phone dies at the end of the conversation.")
         else:
-        print("You successfully make the phone call.")
-        print("Your phone is now at %s" % self.battery_left)
+            print("You successfully make the phone call.")
+            print("Your phone is now at %s" % self.battery_left)
 
+   def smash_phone(self):
+        print("SMASH!!!!!!!")
+        print("It Broke.")
+        self.screen = False
 
-my_phone = Phone("ATT, 100")
-your_phone = Phone{"Bell", 0}
+# Initialize Objects
+my_phone = Phone("ATT", 100)
+your_phone = Phone("Bell", 0)
+default_phone = Phone("Verizon")
 
+my_phone.make_call(60)
+my_phone.make_call(10)
+my_phone.make_call(100)
+my_phone.make_call(10)
+your_phone.smash_phone()
+your_phone.make_call(1)
+print(Special_Random.RandomWiebe.my_random())
