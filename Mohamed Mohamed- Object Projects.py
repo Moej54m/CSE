a1e635be8481afa8f9a2ffaf5662c6933426c87f
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-02-14 09:45:16 -0800

    2/14/19

---
+++ Mohamed Mohamed- Object Projects.py
@@ -0,0 +1,7 @@
+class Airpod(object):
+    def __init__(self):
+        self.wires = False
+        self.charged = 0
+        self.in_range = True
+        self.water_resistant = True
+        self.case = True
