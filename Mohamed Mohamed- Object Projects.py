07741b2550fcec349dcd5cfe76e510d89e168da4
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-02-22 09:13:15 -0800

    2/22/19

--- Mohamed Mohamed- Object Projects.py
+++ Mohamed Mohamed- Object Projects.py
@@ -10,30 +10,16 @@ class Airpods(object):
         self.battery += time
         if self.battery > 50:
             self.battery = 50
-        return
-        self.battery -= duration * battery_loss_each_minute
-        if self.battery < 0:
-            self.battery = 0
-            print("Your Airpods are dead. It died while you were listening to music.")
-        elif self.battery == 0:
-            print("Your Airpods died after the music ended.")
-        else:
-            print ("You have listened to all the music you want.")
-            print("Your Airpod's battery is now %s" % self.battery)
-            return
-            battery_loss_each_minute = 3
-            if self.battery <= 0:
-                print("You can't use them. The battery is dead.")
 
     def case(self):
         print("Congratulations. Your Airpods are now protected in the case.")
         self.protection = True
 
-    def listen_music(self, duration):
-            if not self.in_range:
-                print("You can't listen to music.")
-                print("Your Airpods are not in range.")
-                self.in_range = False
+    def listen_music(self):
+        if not self.in_range:
+            print("You can't listen to music.")
+            print("Your Airpods are not in range.")
+            self.in_range = False
 
     def water_resistant(self):
         print("Oh NOOO. You dropped your Airpods in the pool. Luckily, they are water resistant.")
