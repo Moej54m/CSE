1f745b147a53101e1834311116b895f8a3c0dec0
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-03-13 09:46:01 -0700

    Items 3/13

--- Mohamed Mohamed - Items.py
+++ Mohamed Mohamed - Items.py
@@ -2,6 +2,37 @@ class Item(object):
     def __init__(self, name):
         self.name = name
 
-class Weapon(item):
-    def __init__(self, damage, name, durability):
-        super(we)
+
+class Weapon(Item):
+    def __init__(self, name, damage, durability):
+        super(Weapon, self). __init__(name)
+        self.damage = damage
+        self.durability = durability
+
+
+def use_weapon(self):
+        self.durabilty -= 10
+        print("You use the weapon and its durability drops down by 10.")
+
+
+class Sword(Weapon):
+    def __init__(self):
+        super(Sword, self).__init__("Sword", 15, 80)
+
+
+class Knife(Weapon):
+    def __init__(self):
+        super(Knife, self).__init__("Knife", 7, 50)
+
+
+class Assault_Rifle(Weapon):
+    def __init__(self):
+        super(Assault_Rifle, self).__init__("Assault_Rifle", 20, 100)
+
+    def shoot(self):
+        self.durabilty -= 3
+        print("You fire your assault rifle.")
+
+
+
+
