9ab260e6d89568d893937fac5a70c5f7f75f6288
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-03-28 09:45:14 -0700

    3/28/19

--- WORLD MAP OOP- Mohamed Mohamed.py
+++ WORLD MAP OOP- Mohamed Mohamed.py
@@ -1,5 +1,6 @@
+
 class Room(object):
-    def __init__(self, name, north=None, south=None, east=None, west=None, up=None, down=None, description=()):
+    def __init__(self, name, north=None, south=None, east=None, west=None, up=None, down=None, description=(), item=None):
         self.name = name
         self.north = north
         self.south = south
@@ -8,8 +9,174 @@ class Room(object):
         self.up = up
         self.down = down
         self.description = description
+        self.item = item
 
 
+class Item(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Weapon(Item):
+    def __init__(self, name: object, damage: object, durability: object) -> object:
+        super(Weapon, self). __init__(name)
+        self.damage = damage
+        self.durability = durability
+
+
+def use_weapon(self):
+        self.durabilty -= 10
+        print("You use the weapon and its durability drops down by 10.")
+
+
+class BoneSaw(Weapon):
+    def __init__(self):
+        super(BoneSaw, self). __init__("Bone Saw", 20, 60)
+
+
+class Sword(Weapon):
+    def __init__(self):
+        super(Sword, self).__init__("Sword", 20, 80)
+
+
+class Sniper(Weapon):
+    def __init__(self):
+        super(Sniper, self). __init__("Sniper", 50, 100)
+
+
+class SMG(Weapon):
+    def __init__(self):
+        super(SMG, self). __init__("SubMachine Gun", 15, 100)
+
+
+class Rock(Weapon):
+    def __init__(self):
+        super(Rock, self). __init__("Rock", 5, 20)
+
+
+class Spoon(Weapon):
+    def __init__(self):
+        super(Spoon, self). __init__("Spoon", 3, 10)
+
+
+class Knife(Weapon):
+    def __init__(self):
+        super(Knife, self).__init__("Knife", 15, 50)
+
+
+class AssaultRifle(Weapon):
+    def __init__(self):
+        super(AssaultRifle, self).__init__("Assault Rifle", 30, 100)
+
+    def shoot(self):
+        self.durability -= 3
+        print("You fire your assault rifle. Be careful, the durability is going down every shot only for this rifle.")
+
+
+class Armor(Item):  # ARMOR DEFINITION
+    def __init__(self, name, defense):
+        super(Armor, self). __init__(name)
+        self.defense = defense
+
+
+class ChestPiece(Armor):
+    def __init__(self):
+        super(ChestPiece, self). __init__("Chest Armor", 40)
+
+
+class Gauntlets(Armor):
+    def __init__(self):
+        super(Gauntlets, self). __init__("Gauntlets", 20)
+
+
+class Greaves(Armor):
+    def __init__(self):
+        super(Greaves, self). __init__("Greaves", 10)
+
+
+class ShoulderPlates(Armor):
+    def __init__(self):
+        super(ShoulderPlates, self).__init__("Shoulder Plates", 10)
+
+
+class Helmet(Armor):
+    def __init__(self):
+        super(Helmet, self). __init__("Helmet", 20)
+
+
+class Shield(Item):                         # SHIELD DEFINITION
+    def __init__(self, name, health, durability):
+        super(Shield, self).__init__(name)
+        self.durability = durability
+        self.health = health
+
+
+class HUGESHIELD(Shield):
+    def __init__(self):
+        super(HUGESHIELD, self). __init__("Huge Shield", 50, 99999999999999999999)
+
+    def drink_shield(self):
+        self.durability -= 1
+        self.Shield += 50
+
+
+class SMALLSHIELD(Shield):
+    def __init__(self):
+        super(SMALLSHIELD, self). __init__("Huge Shield", 25, 999999999999999999999)
+
+    def drink_shield(self):
+        self.durability -= 1
+        self.shield += 25
+
+
+class Character(object):
+    def __init__(self, name: str, health: int, weapon, armor):
+        self.name = name
+        self.health = health
+        self.weapon = weapon
+        self.armor = armor
+
+    def take_damage(self, damage: int):
+        if self.armor.defense > damage:
+            print("No damage is done because of some extravagant armor.")
+        else:
+            self.health -= damage - self.armor.defense
+        print("%s has %d health left" % (self.name, self.health))
+
+    def attack(self, target):
+        print("%s attacks %s for %d damage" % (self.name, target.name, self.weapon.damage))
+        target.take_damage(self.weapon.damage)
+
+
+# Items
+Sword = Weapon("Sword", 30, 100)
+SMG = Weapon("SubMachineGun", 15, 100)
+ChestPiece = Armor("Chest Armor", 40)
+
+# Characters
+Zombie = Character("Zombie", 100, Sword, Armor("Generic Armor", 2))
+Ghost = Character("Ghost", 10000, SMG, ChestPiece)
+
+Zombie.attack(Ghost)
+Ghost.attack(Zombie)
+Ghost.attack(Zombie)
+Ghost.attack(Zombie)
+
+assault_rifle = AssaultRifle()
+bone_saw = BoneSaw
+sword = Sword
+sniper = Sniper
+smg = SMG
+rock = Rock
+spoon = Spoon
+knife = Knife
+chest_piece = ChestPiece
+gauntlets = Gauntlets
+greaves = Greaves
+shoulder_plates = ShoulderPlates
+helmet = Helmet
+hugeshield = HUGESHIELD
+smallshield = SMALLSHIELD
 class Player(object):
     def __init__(self, starting_location):
         self.current_location = starting_location
@@ -106,8 +273,10 @@ DOCTOR_ROOM = Room('The Doctor Room', None, None, None, 'Laundry Room', None, No
 
 player = Player(LOBBY)
 
-playing =
-directions = ['north', 'south', 'east', 'west', 'up', 'down']True
+
+
+playing = True
+directions = ['north', 'south', 'east', 'west', 'up', 'down']
 
 while playing:
     print(player.current_location.name)
