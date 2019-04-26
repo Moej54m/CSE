08bb4ed26c9e34720a7bc1be3770c25a54154120
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-04-26 09:48:21 -0700

    4/26/19

--- CSV Notes.py
+++ CSV Notes.py
@@ -8,8 +8,53 @@ with open("Book1.csv") as old_csv:
         for row in reader:
             old_number = row[0]
             first_num = int(old_number[0])
-            if first_num % 2 == 0:
-        writer.writerow(row)
+            if first_num % 3 == 0:
+                writer.writerow(row)
         # old_number = int(row[0]) + 1
         # print(old_number)
 print("OK")
+
+
+def validate(num: str):
+    first_num = int(num[0])
+    if first_num % 3 == 0:
+        return True
+    return False
+
+
+def divisible_by_2(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 0:
+        return True
+    return False
+
+
+def divisible_by _3(num: str):
+    first_num = int(num[0])
+    if first_num % 3 == 0:
+        return True
+    return False
+
+    
+    def all_16_digits(num: str):
+        if len(num) == 16:
+            return True
+        else:
+            print(" NOT EVERY NUMBER IS 16 DIGITS")
+            return False
+
+# import csv# #
+# with open("Book1.csv") as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_CSV:
+#         print("Writing file.... ")
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         for row in reader:
+#             old_number = row[0]
+#             first_num = int(old_number[0])
+#             if first_num % 2 == 0:
+#         writer.writerow(row)
+#         # old_number = int(row[0]) + 1
+#         # print(old_number)
+# print("OK")
+#
