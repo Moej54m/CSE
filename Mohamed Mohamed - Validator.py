b13813fca3798880e9f09ab01b92100b15ce104c
Moej54m <42385567+Moej54m@users.noreply.github.com>
2019-05-03 10:41:19 -0700

    5/3/19

--- Mohamed Mohamed - Validator.py
+++ Mohamed Mohamed - Validator.py
@@ -1,23 +1,28 @@
-def valid_card_number(num: str):
-    return False
+# The Luhn Formula:
+#
+# Drop the last digit from the number. The last digit is what we want to check against
+# Reverse the numbers
+# Multiply the digits in odd positions (1, 3, 5, etc.) by 2 and subtract 9 to all any result higher than 9
+# Add all the numbers together
+# The check digit (the last number of the card) is the amount that you would need to add to get a multiple of 10 (Modulo 10)
+
+def validate (num: str):
+    if not all_16_digits(num):
+        drop_num("3785162546340070")
+        reverse("3785162546340070")
+
+def reverse(num: str):
+    list_num = list(num)
+    print(list_num(:: -1))
 
 
 def all_16_digits(num: str):
     if len(num) == 16:
-        return True
-    else:
-        print(" NOT EVERY NUMBER IS 16 DIGITS")
-        return False
+        print("Every card has 16 digits.")
 
 
-def validate(num: str):
-    if not all_16_digits(num):
-        return False
-
-
-print(validate("8248714205534850"))
-
-
-def drop_last_num(num: str):
+def drop_num(num: str):
     list_num = list(num)
     for index in range(len(list_num)):
+        list_num[index] = int(list_num[index])
+        list_num.pop(15)
