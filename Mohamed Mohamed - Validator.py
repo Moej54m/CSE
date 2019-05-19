224216494caefdd8fc699ea82dd7c296b854d0fe
Moe Ali <Moeali@Moes-MacBook-Pro.local>
2019-05-18 17:05:43 -0700

    5/18/19

--- Mohamed Mohamed - Validator.py
+++ Mohamed Mohamed - Validator.py
@@ -6,4 +6,7 @@ def sixteen_digits(number16: str):
         if len(number16) == 16:
             return True
         else:
+            # print("Not every number is 16 digits!)
+            return False
+
 
